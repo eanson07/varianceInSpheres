ae312be810189541081a8ba0d889191e25559356
Erik Anson <eanson07@gmail.com>
2015-10-18 05:46:42 -0500

    Running on Stampede

--- hack_hash.cc
+++ hack_hash.cc
@@ -1,31 +1,21 @@
 #include <functional>
 #include <thread>
+#include <iostream>
+#include <string>
 #include <sstream>
 
 namespace std
 {
 
-  /*
-  template<>
-  static  hash<std::thread::id>
-  {
-   public:
-    size_t operator()(const std::thread::id &val) const
-    {
-      stringstream ss;
-      ss << val;
-      return (size_t) stol(ss.str());
-    }
-
-  };
-  */
-
   template <> 
   size_t hash<thread::id>::operator()(const thread::id val) const
   {
     stringstream ss;
+    auto hasher = hash<string>();
     ss << val;
-    return (size_t) stol(ss.str());
+    cerr << val << endl;
+    return hasher(ss.str());
+    //    return (size_t) stol(ss.str());
   }
 
 
